c7e565fe7b054efe628d515ed77d7f2b6b8a93f1
Dou Yutao <yutaodou@gmail.com>
2020-11-08 11:23:34 +0800

    window mgmt works

---
+++ hammerspoon/init.lua
@@ -0,0 +1,30 @@
+-- global configuration
+hs.window.animationDuration = 0.1
+
+-- reload configuration
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
+    hs.reload()
+end)
+hs.alert.show("Hammerspoon config loaded")
+
+
+-- config WinWin
+hs.loadSpoon("WinWin")
+
+winwinHotkeyMapping = {
+    left = "halfleft",
+    right = "halfright",
+    up ="halfup",
+    down = "halfdown",
+    m = "maximize",
+    f = "fullscreen",
+    s = "shrink",
+    n = "next",
+    c = "center"
+}
+
+for k, action in pairs(winwinHotkeyMapping) do
+    hs.hotkey.bind({"cmd", "alt", "ctrl"}, k, function() 
+        spoon.WinWin:moveAndResize(action)
+    end)
+end
